e933486ab8ae76953dccf723ef6dec0086f50c5d
Race Osuna <119973475+RaceOsuna@users.noreply.github.com>
2023-07-21 08:55:34 -0600

    Refactor: details display

--- src/GameDisplay.js
+++ src/GameDisplay.js
@@ -23,11 +23,10 @@ const [game, setGame] = useState([])
           <img src={game.images.thumb} alt={`${game.handle} thumbnail`} />
         </div> 
         <div className='details'>
-          <h1>{game.handle}</h1>
+          <h4 style={{backgroundColor: 'black', color: 'lightGreen'}}>{game.msrp_text}</h4>
           <h4 style={{backgroundColor: 'black', color: 'lightBlue'}}>{game.players} Players</h4>
           <h4 style={{backgroundColor: 'black', color: 'red'}}>ages {game.min_age}+</h4>
           <h4 style={{backgroundColor: 'black', color: 'yellow'}}>{game.average_user_rating.toFixed(2)} ⭐️</h4>
-          <h4 style={{backgroundColor: 'black', color: 'lightGreen'}}>{game.msrp_text}</h4>
         </div> 
       </div>
       <div className="links">

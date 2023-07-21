8ef009aa72b283aba061e4ab285e22586393ba91
Race Osuna <119973475+RaceOsuna@users.noreply.github.com>
2023-07-20 22:16:35 -0600

    Feat: h3 style, nav isActive style

--- src/GameDisplay.js
+++ src/GameDisplay.js
@@ -23,15 +23,15 @@ const [game, setGame] = useState([])
           <img src={game.images.thumb} alt={`${game.handle} thumbnail`} />
         </div> 
         <div className='intro'>
-          <h1>{game.handle}</h1>
-          <h3>{game.players} Players</h3>
-          <h3>ages {game.min_age}+</h3>
-          <h3>{game.average_user_rating.toFixed(2)} ⭐️</h3>
+          <h1 style={{backgroundColor: 'black', color: 'lightGreen'}}>{game.handle}</h1>
+          <h3 style={{backgroundColor: 'black', color: 'lightBlue'}}>{game.players} Players</h3>
+          <h3 style={{backgroundColor: 'black', color: 'red'}}>ages {game.min_age}+</h3>
+          <h3 style={{backgroundColor: 'black', color: 'yellow'}}>{game.average_user_rating.toFixed(2)} ⭐️</h3>
         </div> 
       </div>
       <div className="links">
         <nav className='game-nav'>
-          <NavLink to=".">About</NavLink>
+          <NavLink to="." end className={({isActive}) => isActive ? 'active' : 'game-nav a'}>About</NavLink>
           <NavLink to="/game/rules">Rules</NavLink>
           <NavLink to="/game/reviews">Reviews</NavLink>
         </nav>

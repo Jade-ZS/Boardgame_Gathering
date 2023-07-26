9f6a39f99a4d97c062c3db3cbab3af7ae3c33c2b
Race Osuna <119973475+RaceOsuna@users.noreply.github.com>
2023-07-26 12:43:20 -0600

    Refactor: pop menu styling

--- src/PopOutMenu.js
+++ src/PopOutMenu.js
@@ -1,12 +1,32 @@
 import React from "react";
 import { useSearchParams } from "react-router-dom";
 import "./PopOutMenu.css";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import { useState } from "react";
 
 
 
 function PopOutMenu({ games, handleClose, }) {
+
+  // const [gameList, setGameList] = useState([]);
+  // const [show, setShow] = useState(false);
+
+  // function filterGamesByType(event) {
+  //   const gameType = games.filter(game => game.categories.some(cat => cat.id === event.target.id))
+  //   setGameList(gameType)
+  //   setShow(true)
+  // }
+
+  // function filterGamesByPlayers(event) {
+  //   const numPlayers = games.filter(game => game.min_players === Number(event.target.value) || game.max_players > Number(event.target.value))
+  //   setGameList(numPlayers)
+  //   setShow(true)
+  // }
+
+  // function clearFilters() {
+  //   setGameList([])
+  //   setShow(false)
+  // }
 
   const [searchParams, setSearchParams] = useSearchParams();
  
@@ -34,35 +54,37 @@ function PopOutMenu({ games, handleClose, }) {
   return (
       <div className="pop-out-menu">
         <div className="filtered">
-          {show && <ul className="filter-ul">
-            <p>Your Options</p>
+          {show && <div className="filter-ul">
             <button onClick={clearFilters}>Clear Filters</button>
+            <p className="label">Your Options</p>
             {yearPublished && yearList.map(game => (
               <Link to={`/${game.id}`}>
                 <div key={game.id} className="option">
-                  <li>👾 {game.handle}</li>
+                  <p>👾</p>
+                  <li>{game.handle.replaceAll('-', ' ')}</li>
                 </div>
               </Link>
             ))}
             {gameType && gameList.map(game => (
-              <Link to={`/${game.id}`}>
+              <Link to={`/${game.id}`} className="game-link">
                 <div key={game.id} className="option">
-                  <li>👾 {game.handle}</li>
+                  <p>👾</p>
+                  <li>{game.handle.replaceAll('-', ' ')}</li>
                 </div>
               </Link>
             ))}
-          </ul>}
+          </div>}
         </div>
         <div>
-        <h4 onClick={handleClose}>Close</h4>
+        <button onClick={handleClose}>Close</button>
         <div className="menu-item">
           <p className="label">Game Type</p>
           <div className="drop-down-menu">
             <ul>
-              <li id='nuHYRFmMjU' onClick={(event) => handleClick(event, "game_type")}>Renaissance</li>
-              <li id="KUBCKBkGxV" onClick={(event) => handleClick(event, "game_type")}>Adventure</li>
-              <li id="ge8pIhEUGE" onClick={(event) => handleClick(event, "game_type")}>Cooperative</li>
-              <li id="JwHcKqxh33" onClick={(event) => handleClick(event, "game_type")}>Trains</li>
+              <li id='nuHYRFmMjU' className="filter-li" onClick={(event) => handleClick(event, "game_type")}>Renaissance</li>
+              <li id="KUBCKBkGxV" className="filter-li" onClick={(event) => handleClick(event, "game_type")}>Adventure</li>
+              <li id="ge8pIhEUGE" className="filter-li" onClick={(event) => handleClick(event, "game_type")}>Cooperative</li>
+              <li id="JwHcKqxh33" className="filter-li" onClick={(event) => handleClick(event, "game_type")}>Trains</li>
             </ul>
           </div>
         </div>
@@ -70,18 +92,18 @@ function PopOutMenu({ games, handleClose, }) {
           <p className="label">Year Published</p>
           <div className="drop-down-menu">
             <ul>
-              <li id="2010" onClick={(event) => handleClick(event, "year_published")}>2010</li>
-              <li id="2011" onClick={(event) => handleClick(event, "year_published")}>2011</li>
-              <li id="2012" onClick={(event) => handleClick(event, "year_published")}>2012</li>
-              <li id="2013" onClick={(event) => handleClick(event, "year_published")}>2013</li>
-              <li id="2014" onClick={(event) => handleClick(event, "year_published")}>2014</li>
-              <li id="2015" onClick={(event) => handleClick(event, "year_published")}>2015</li>
-              <li id="2016" onClick={(event) => handleClick(event, "year_published")}>2016</li>
-              <li id="2017" onClick={(event) => handleClick(event, "year_published")}>2017</li>
-              <li id="2018" onClick={(event) => handleClick(event, "year_published")}>2018</li>
-              <li id="2019" onClick={(event) => handleClick(event, "year_published")}>2019</li>
-              <li id="2020" onClick={(event) => handleClick(event, "year_published")}>2020</li>
-              <li id="2021" onClick={(event) => handleClick(event, "year_published")}>2021</li>
+              <li id="2010" className="filter-li" onClick={(event) => handleClick(event, "year_published")}>2010</li>
+              <li id="2011" className="filter-li" onClick={(event) => handleClick(event, "year_published")}>2011</li>
+              <li id="2012" className="filter-li" onClick={(event) => handleClick(event, "year_published")}>2012</li>
+              <li id="2013" className="filter-li" onClick={(event) => handleClick(event, "year_published")}>2013</li>
+              <li id="2014" className="filter-li" onClick={(event) => handleClick(event, "year_published")}>2014</li>
+              <li id="2015" className="filter-li" onClick={(event) => handleClick(event, "year_published")}>2015</li>
+              <li id="2016" className="filter-li" onClick={(event) => handleClick(event, "year_published")}>2016</li>
+              <li id="2017" className="filter-li" onClick={(event) => handleClick(event, "year_published")}>2017</li>
+              <li id="2018" className="filter-li" onClick={(event) => handleClick(event, "year_published")}>2018</li>
+              <li id="2019" className="filter-li" onClick={(event) => handleClick(event, "year_published")}>2019</li>
+              <li id="2020" className="filter-li" onClick={(event) => handleClick(event, "year_published")}>2020</li>
+              <li id="2021" className="filter-li" onClick={(event) => handleClick(event, "year_published")}>2021</li>
             </ul>
           </div>
           {/* <ul>

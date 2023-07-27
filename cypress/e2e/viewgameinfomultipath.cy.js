efeaf1a2fa0ddf3bd239af0df2745e52287307a0
Victor Sriqui <victorsriqui@gmail.com>
2023-07-27 15:08:44 -0600

    test: Game infos game details should have correct contents, attributes, and elements.

--- cypress/e2e/viewgameinfomultipath.cy.js
+++ cypress/e2e/viewgameinfomultipath.cy.js
@@ -72,6 +72,7 @@ describe('template spec', () => {
 
     })
   })
+
   it('User should be able to select a game from party games carousel, view game info, and return home', () => {
 
     cy.visit('http://localhost:3000/')
@@ -90,6 +91,7 @@ describe('template spec', () => {
 
     })
   })
+
   it('User should be able to select a game from kid friendly carousel, view game info, and return home', () => {
 
     cy.visit('http://localhost:3000/')
@@ -108,6 +110,7 @@ describe('template spec', () => {
 
     })
   })
+
   it('Home button should be present, and have correct contents.', () => {
 
     cy.visit('http://localhost:3000/')
@@ -129,6 +132,59 @@ describe('template spec', () => {
       .contains('⌂')
       })
     })
+  })
+  
+  it('Game details should be present, and have correct contents.', () => {
+
+    cy.visit('http://localhost:3000/')
+    cy.get('.game-row')
+    .eq(2)
+    .within(() => {
+      cy.get('img')
+      .first()
+      .click()
+      })
+    cy.url()
+    .should('include', '/3hnL2wtWnM')
+    
+    cy.get('.game-details')
+    .within(()=>{
+      cy.get('.thumbnail')
+      .within(()=>{
+        cy.get('img')
+        .should('have.attr', 'src')
+        .should('include', 'https://d2k4q26owzy373.cloudfront.net/350x350/games/uploaded/1594689503033') 
+        cy.get('img')
+        .should('have.attr', 'alt')
+        .should('include', 'chess thumbnail') 
+        
+      })
+      cy.get('.details')
+        .each(()=>{
+          cy.get('h4')
+          .eq(0)
+          .contains('14.95')
+          .should('have.attr', 'style')
+          .should('include', 'lightgreen')
+          cy.get('h4')
+          .eq(1)
+          .contains('2 Players')
+          .should('have.attr', 'style')
+          .should('include', 'lightblue')
+          cy.get('h4')
+          .eq(2)
+          .contains('Ages 6+')
+          .should('have.attr', 'style')
+          .should('include', 'red')
+          cy.get('h4')
+          .eq(3)
+          .contains('3.32 ⭐️')
+          .should('have.attr', 'style')
+          .should('include', 'yellow')
+        })
+      })
+    
 
   })
+
 })

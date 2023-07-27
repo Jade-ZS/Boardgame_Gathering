339df4220c2b168e470a3b9849f0ddcd73b303e5
Jade Shi <ziyushi1997@gmail.com>
2023-07-27 07:42:25 -0700

    Comment out service worker for now

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,49 +1,52 @@
-const gameBoardsCache = "GameBoard Cache";
-const filesPath = [
-  '/',
-  "/index.html",
-  "/App.js",
-  "/index.js",
-];
+// const gameBoardsCache = `GameBoard Cache version`;
+// const filesPath = [
+//   '/',
+//   "/index.html",
+//   "/App.js",
+//   "/index.js",
+// ];
 
-const self = this;
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(gameBoardsCache).then((cache) => {
-      console.log("Open Cache");
-      return cache.addAll(filesPath);
-    })
-  );
-});
+// const self = this;
+// self.addEventListener("install", (event) => {
+//   event.waitUntil(
+//     caches
+//       .open(gameBoardsCache)
+//       .then((cache) => {
+//         console.log("Open Cache");
+//         return cache.addAll(filesPath);
+//       })
+    
+//   );
+// });
 
 
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request)
-      .then((response) => {
-        if (response) {
-          return response;
-        } 
-        return fetch(event.request)
-          .then(response => {
-            const responseClone = response.clone();
-            caches.open(gameBoardsCache)
-              .then(cache => cache.put(event.request.url, responseClone))
-            return response;
-          })
-      .catch()
-    })
-  );
-});
+// self.addEventListener("fetch", (event) => {
+//   event.respondWith(
+//     caches.match(event.request)
+//       .then((response) => {
+//         if (response) {
+//           return response;
+//         } 
+//         return fetch(event.request)
+//           .then(response => {
+//             const responseClone = response.clone();
+//             caches.open(gameBoardsCache)
+//               .then(cache => cache.put(event.request.url, responseClone))
+//             return response;
+//           })
+//       .catch()
+//     })
+//   );
+// });
   
-self.addEventListener("activate", () => {
-  console.log("Activation");
-  caches.keys()
-    .then(keys => {
-      keys.map(key => {
-        if(key !== gameBoardsCache) {
-          caches.delete(key);
-        }
-      })
-    })
-});
+// self.addEventListener("activate", () => {
+//   console.log("Activation");
+//   caches.keys()
+//     .then(keys => {
+//       keys.map(key => {
+//         if(key !== gameBoardsCache) {
+//           caches.delete(key);
+//         }
+//       })
+//     })
+// });

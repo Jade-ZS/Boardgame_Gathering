7f57e03f7e68f35d039aa4055029853c48bce211
Jade Shi <ziyushi1997@gmail.com>
2023-07-27 14:24:42 -0700

    Add offline html to service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,52 +1,53 @@
-// const gameBoardsCache = `GameBoard Cache version`;
-// const filesPath = [
-//   '/',
-//   "/index.html",
-//   "/App.js",
-//   "/index.js",
-// ];
+const gameBoardsCache = `GameBoard Cache version`;
+const filesPath = [
+  '/',
+  "/index.html",
+  "/App.js",
+  "/index.js",
+  "/offline.html"
+];
 
-// const self = this;
-// self.addEventListener("install", (event) => {
-//   event.waitUntil(
-//     caches
-//       .open(gameBoardsCache)
-//       .then((cache) => {
-//         console.log("Open Cache");
-//         return cache.addAll(filesPath);
-//       })
+const self = this;
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches
+      .open(gameBoardsCache)
+      .then((cache) => {
+        console.log("Open Cache");
+        return cache.addAll(filesPath);
+      })
     
-//   );
-// });
+  );
+});
 
 
-// self.addEventListener("fetch", (event) => {
-//   event.respondWith(
-//     caches.match(event.request)
-//       .then((response) => {
-//         if (response) {
-//           return response;
-//         } 
-//         return fetch(event.request)
-//           .then(response => {
-//             const responseClone = response.clone();
-//             caches.open(gameBoardsCache)
-//               .then(cache => cache.put(event.request.url, responseClone))
-//             return response;
-//           })
-//       .catch()
-//     })
-//   );
-// });
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          return response;
+        } 
+        return fetch(event.request)
+          .then(response => {
+            const responseClone = response.clone();
+            caches.open(gameBoardsCache)
+              .then(cache => cache.put(event.request.url, responseClone))
+            return response;
+          })
+      .catch()
+    })
+  );
+});
   
-// self.addEventListener("activate", () => {
-//   console.log("Activation");
-//   caches.keys()
-//     .then(keys => {
-//       keys.map(key => {
-//         if(key !== gameBoardsCache) {
-//           caches.delete(key);
-//         }
-//       })
-//     })
-// });
+self.addEventListener("activate", () => {
+  console.log("Activation");
+  caches.keys()
+    .then(keys => {
+      keys.map(key => {
+        if(key !== gameBoardsCache) {
+          caches.delete(key);
+        }
+      })
+    })
+});

ea1c55b7124ed8e6bc1ff0c3de77363f32bd0b15
Jade Shi <ziyushi1997@gmail.com>
2023-07-29 09:09:06 -0700

    Remove comments

--- src/Card.js
+++ src/Card.js
@@ -5,7 +5,6 @@ import { Link } from 'react-router-dom';
 export default function Card({name ,imgSrc, id}) {
 
   return (
-    // To Do: add link path
     <Link to={`/${id}`}>
     <div className='card'>
       <img src={imgSrc}/>
